707dc3af9de8c477fbba74e7693f14ae292775e8
psvh3 <pearsonvh@gatech.edu>
2016-07-23 21:37:41 +0200

    Made new base queries that you can copy and paste
    Use these queries extended queries. They should help you just copy and paste in and then insert variable names.

--- EXT_Queries.sql
+++ EXT_Queries.sql
@@ -4,6 +4,7 @@
 SELECT *
 FROM COUNTRY
 WHERE Population < 10000000 and Population > 0;
+
 # Select country names where they speak more than one specified language
 	# Just add another lang_name and increase the HAVING statement by one 
     # if you want to add more languages needed
@@ -21,3 +22,93 @@ FROM COUNTRY;
 -------------------
 # City Queries
 
+# Select the cities in a certain country
+SELECT Country_Name, City_Name, City_Population
+FROM CITY
+WHERE Country_Name = 'France';
+
+# Select City by Population
+SELECT Country_Name, City_Name, City_Population
+FROM CITY
+WHERE City_Population < 2500000;
+
+# Search Cities by Score
+SELECT Country_Name, City_Name, AVG_Score
+FROM AVE_CITY_SCORE
+WHERE AVG_Score > 2;
+
+# Select city names where they speak more than one specified language
+	# Just add another lang_name and increase the HAVING statement by one 
+    # if you want to add more languages needed
+SELECT Country_Name, City_Name
+FROM City_Language 
+WHERE Lang_Name = 'Spanish' or Lang_Name = 'Catalan'
+GROUP BY City_Name
+HAVING Count(*) > 1
+ORDER BY City_Name;
+
+---------------------------------
+# Location Queries
+
+# Search Locations by Country
+SELECT * 
+FROM LOCATION
+WHERE Country_Name = 'Spain';
+
+# Search Locations by City
+SELECT * 
+FROM LOCATION
+WHERE City_Name = 'Barcelona';
+
+# Searching Locations by score
+SELECT *
+FROM AVE_LOCATION_SCORE
+WHERE AVG_Score > 2;
+
+# Search by Category
+Select *
+FROM LOCATION
+WHERE Loc_Type = 'Church';
+
+# Search by Category and City
+Select *
+FROM LOCATION
+WHERE Loc_Type = 'Church' AND City_Name = 'Paris';
+
+# Search by Category and Score
+Select *
+FROM LOCATION NATURAL JOIN AVE_LOCATION_SCORE
+WHERE Loc_Type = 'Other' and AVG_Score > 3;
+
+---------------------------------
+# Event Queries
+
+# Searching events by country
+SELECT *
+FROM EVENT
+WHERE Country_Name = 'Spain';
+
+# Searching events by city
+SELECT *
+FROM EVENT
+WHERE City_Name = 'Barcelona';
+
+# Searching events by score
+SELECT *
+FROM AVE_EVENT_SCORE
+WHERE AVG_Score > 2;
+
+# Search by Category
+Select *
+FROM EVENT
+WHERE Category = 'Concert';
+
+# Search by Category and City
+Select *
+FROM EVENT
+WHERE Category = 'Concert' AND City_Name = 'Paris';
+
+# Search by Category and Score
+Select *
+FROM EVENT NATURAL JOIN AVE_EVENT_SCORE
+WHERE Category = 'Concert' and AVG_Score > 2;
