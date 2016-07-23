4c9b5c1d74b836c858978ff2862558fe7159784a
cptarnie <jonathan.boutsicaris@gmail.com>
2016-07-23 19:17:34 +0200

    Add files via upload
    added new register file and temporary login file

---
+++ www/login.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	<body>
+		<h1>
+			Login
+		</h1>
+
+		<form action="homepage.html" method = "post">
+
+			<p>Username:
+				<input type="text" name="username" size="30" value="" />
+			</p>
+
+			<p>Password:
+				<input type="text" name="password" size="30" value="" />
+			</p>
+
+			<p>
+			<input type = "submit" name = "submit" value = "Send" />
+			</p>
+
+		</form>
+	</body>
+</html>
